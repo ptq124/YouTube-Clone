6c1f35a1b049859ffeb149b05e2a4b1827c0b9bc
박태규 Park Tae Kyu <ptq124@naver.com>
2023-01-26 23:59:57 +0900

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#FF0000'
+      }
+    },
+  },
+  plugins: [],
+}
