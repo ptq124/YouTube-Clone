a5c18b01e4a778ce158e692c450d39baef0d8702
박태규 Park Tae Kyu <ptq124@naver.com>
2023-01-27 23:57:48 +0900

    feat: react router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import Videos from './pages/Videos';
+import VideosDetail from './pages/VideosDetail';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {index: true, element: <Videos />},
+      {path: 'videos', element: <Videos />},
+      {path: 'videos/:keyword', element: <Videos />},
+      {path: 'videos/watch/:videoId', element: <VideosDetail />},
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/pages/Videos.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function Videos() {
+  const {keyword} = useParams()
+  return (
+    <div>
+      videos{keyword}
+    </div>
+  );
+}
+
