0660d596f911c6b7d03ba7820d8e1fb634d13556
박태규 Park Tae Kyu <ptq124@naver.com>
2023-02-01 03:14:17 +0900

    Update youtubeClient.js

--- src/api/youtubeClient.js
+++ src/api/youtubeClient.js
@@ -4,7 +4,7 @@ export default class YoutubeClient {
   constructor() {
     this.httpClient = axios.create({
       baseURL: 'https://www.googleapis.com/youtube/v3',
-      params: { key: process.env.REACT_APP_YOUTUBE_API_KEY },
+      params: { key: process.env.REACT_APP_API_KEY },
     });
   }
 
