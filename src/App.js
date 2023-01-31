45e834dc9bf74a937a937cb54327383909d9008f
박태규 Park Tae Kyu <ptq124@naver.com>
2023-02-01 02:45:39 +0900

    fix: react query update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import { YoutubeApiProvider } from './context/YoutubeApiContext';
