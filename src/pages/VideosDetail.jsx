110bac20c3f7ecbc2d7159226e7342b2c697c262
박태규 Park Tae Kyu <ptq124@naver.com>
2023-02-01 02:48:17 +0900

    feat: 비디오 컴포넌트 추가

--- src/pages/VideosDetail.jsx
+++ src/pages/VideosDetail.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import ChannelInfo from '../components/ChannelInfo';
+import RelatedVideos from '../components/RelatedVideos';
 
 export default function VideosDetail() {
+  const {
+    state: {video},
+  } = useLocation()
+  const {title, channelId, channelTitle, description} = video.snippet
   return (
-    <div>
-      aa
-    </div>
+    <section className='flex flex-col lg:flex-row'>
+      <article className='basis-4/6'>
+        <iframe 
+        id="player" 
+        type="text/html" 
+        width="100%" 
+        height="640"
+        src={`https://www.youtube.com/embed/${video.id}`}
+        title={title}
+        ></iframe>
+        <div className='p-8'>
+          <h2 className='text-xl font-bold'>{title}</h2>
+          <ChannelInfo id={channelId} name={channelTitle}/>
+          <pre className='whitespace-pre-wrap'>{description}</pre>
+        </div>
+      </article>
+      <section className='basis-2/6'>
+        <RelatedVideos id={video.id} />
+      </section>
+    </section>
   );
 }
 
