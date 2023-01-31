1b94ea2ac96a8b343c193c5360a5700fd800f359
박태규 Park Tae Kyu <ptq124@naver.com>
2023-02-01 02:48:33 +0900

    feat: 배포용 api변경

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,12 +1,12 @@
 import { createContext, useContext } from 'react';
-import FakeYoutubeClient from '../api/fakeYotubeClient';
+//import FakeYoutubeClient from '../api/fakeYotubeClient';
 import Youtube from '../api/youtube';
-//import YoutubeClient from '../api/youtubeClient';
+import YoutubeClient from '../api/youtubeClient';
 
 export const YoutubeApiContext = createContext();
 
-const client = new FakeYoutubeClient();
-// const client = new YoutubeClient();
+//const client = new FakeYoutubeClient();
+const client = new YoutubeClient();
 const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }) {
