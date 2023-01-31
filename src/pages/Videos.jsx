cdebf87635eec639317f9c2647157ec32bea0374
박태규 Park Tae Kyu <ptq124@naver.com>
2023-02-01 02:47:36 +0900

    style: grid

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
-import { useQuery } from 'react-query';
+import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 import VideoCard from '../components/common/VideoCard';
-
+import { useYoutubeApi } from '../context/YoutubeApiContext';
 export default function Videos() {
   const {keyword} = useParams()
-  const {isLoading, error, data:videos} = useQuery(
-    ['videos', keyword], async () => {
-      return fetch(`../mock/${keyword ? 'search' : 'popular'}.json`)
-      .then((res)=>res.json)
-      .then((data)=>data.items)
-    }
-  )
-  console.log(videos)
+  const { youtube } = useYoutubeApi();
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(
+    ['videos', keyword], 
+    () => youtube.search(keyword),
+    {staleTime: 1000*60*1}
+  );
   return (
     <>
-      <div>
-        videos{keyword}
-      </div>
         {isLoading && <p>Loading...</p>}
         {error && <p>Someting is wrong</p>}
         {videos && (
-          <ul>
+          <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4'>
             {videos.map((video)=>(
               <VideoCard key={video.id} video={video} />
             ))}
